90aa350c8c0d8e6832d52ffa2dbefdc5559af89d
Bola Malek <malek.bola@gmail.com>
2023-02-14 16:52:16 +0000

    Fix typo

--- deploy.py
+++ deploy.py
@@ -1,3 +1,4 @@
+import baseten
 import truss
 
 # Deploy first model
@@ -7,9 +8,9 @@ sd = truss.load("./sd_v2-1_truss/")
 # sd.docker_predict({"prompt": "a photo of an astronaut riding a horse on mars"})
 
 # Deploy to baseten in one line
-baseten.deploy(sd_img, model_name="Image to Image", publish=True)
+baseten.deploy(sd, model_name="Text to Image", publish=True)
 
-
+# Another Model
 sd_img = truss.load("sd_v2-1_img_truss/")
-sd_img.predict({"prompt": "add a jacket", "image": res["images"][0])
+# sd_img.predict({"prompt": "add a jacket", "image": res["images"][0])
 baseten.deploy(sd_img, model_name="Image to Image", publish=True)
