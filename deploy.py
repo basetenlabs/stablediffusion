ff0859f622e93deb1398ea5756f7bc2ac5f16b30
Amir Haghighat <amir@baseten.co>
2023-02-21 09:01:33 -0800

    Minor docs change

--- deploy.py
+++ deploy.py
@@ -1,26 +1,27 @@
 import os
 
-import baseten
 import truss
 
-baseten.configure("https://app.baseten.co")
-baseten.login(os.environ["BASETEN_API_KEY"])
 
-
-# Truss init. Uncomment the following line to create a new truss
+# Initializing a Truss. Uncomment the following line to create a new Truss
 # truss.create("./difussion_2-1/")
 
-# Got to the `./difussion_2-1/model.py` so that you
-# edit it to load and run predict using your model
+# Got to the `./difussion_2-1/model.py` to edit the
+# Truss: have to fill in the `load` and `predict` functions.
 
-# Deploy first model
+# Load the Truss to memory
 sd = truss.load("./difussion_2-1/")
 
-# Local Testing
+# Local testing
 # sd.docker_predict({"prompt": "a photo of an astronaut riding a horse on mars"})
 
-# Deploy to baseten in one line
+# Deploy to baseten
+import baseten
+baseten.configure("https://app.baseten.co")
+baseten.login(os.environ["BASETEN_API_KEY"])
+# One-line deploy:
 baseten.deploy(sd, model_name="Text to Image", publish=True)
+
 
 # Another Model
 sd_img = truss.load("inpaint_2/")
